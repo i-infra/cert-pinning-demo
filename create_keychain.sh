5749ae75f397eac0146d15b89386d8b95f7329fa
Mike Turner <zaphrod.beeblebrox@gmail.com>
2022-03-23 16:43:16 -0400

    Enable testnet deploys and generation of client secret files (#1)

--- create_keychain.sh
+++ create_keychain.sh
@@ -61,6 +61,7 @@ echo "SERVERCRT=$(cat $output/server.full.pem | gzip - | base64 --wrap=0)" >> $n
 echo "ROOTCRT=$(cat $output/rootcrt.pem | base64 --wrap=0)" > $name.client_secrets
 echo "CLIENTCRT=$(cat $output/client.full.pem | base64 --wrap=0)" >> $name.client_secrets
 
+echo "ROOTCRT=" > $name.clientsecrets && base64 $output/rootcrt.pem >> $name.clientsecrets && echo "CLIENTCRT=" >> $name.clientsecrets && base64 $output/client.full.pem >> $name.clientsecrets && sed -i ':a;N;$!ba;s/\n//g' $name.clientsecrets && sed -i "s/CLIENTCRT/\nCLIENTCRT/g" $name.clientsecrets
 ## might be needed for stunnel verif=3
 #rm -f *.0 || true
 #ln -s rootcrt.pem $(openssl x509 -hash -noout -in rootcrt.pem).0
